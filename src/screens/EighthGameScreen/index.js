8cdb2d9f56e73dcdbad4251732d06778785223fc
still the best <blessedonwebdev@gmail.com>
2020-05-25 07:11:22 +0300

    finished eight screen

--- src/screens/EighthGameScreen/index.js
+++ src/screens/EighthGameScreen/index.js
@@ -10,6 +10,10 @@ const EighthGameScreen = () => {
   const [gameDifficulty, setGameDifficulty] = useState(0);
   const [userScore, setUserScore] = useState(0)
   const [fTime, setFTime] = useState(0);
+  const [userHit, setUserHit] = useState(0);
+  const [userMissClick, setUserMissClick] = useState(0)
+  const [aimResults, setAimResults] = useState([]);
+  const [userScoreArr, setUserScoreArr] = useState([]);
 
   const onRandomShowBlock = useCallback(() => {
     if (isGameNow) {
@@ -55,28 +59,40 @@ const EighthGameScreen = () => {
       setTimeCount(0);
       setIsGameNow(false);
       setGameDifficulty(0)
-
+      setUserHit(0);
+      setUserMissClick(0);
+      setAimResults([]);
+      setUserScoreArr([]);
     }
   }, [timeCount, userScore])
 
   const addScoreAndDelete = (event) => {
+    setUserHit(value => value + 1);
+
     let userAim = (Date.now() - fTime) / 1000;
+
+    setAimResults([...aimResults, userAim]);
 
     switch (true) {
       case (userAim < 0.5) : {
         setUserScore(value => value + 300);
+        setUserScoreArr(value => [...value, 300])
         break;
       }
       case (userAim > 0.5 && userAim < 0.8) : {
         setUserScore(value => value + 200);
+        setUserScoreArr(value => [...value, 200])
         break;
       }
       case (userAim >= 0.8 && userAim < 1.3) : {
         setUserScore(value => value + 100);
+        setUserScoreArr(value => [...value, 200])
+
         break;
       }
       default:
         setUserScore(value => value + 50);
+        setUserScoreArr(value => [...value, 50])
         break;
     }
 
@@ -86,6 +102,8 @@ const EighthGameScreen = () => {
   };
 
   const handleMissingClick = () => {
+    setUserMissClick( value => value + 1);
+
     if (userScore >= 100) {
       setUserScore(value => value - 100);
     } else {
@@ -125,6 +143,11 @@ const EighthGameScreen = () => {
             />
           </div>
           <div>набрано очков {userScore}</div>
+          <div>Попаданий {userHit}</div>
+          <div>Промахов {userMissClick}</div>
+          <div>В среднем за хит {userScoreArr.length > 0 && Math.max(...userScoreArr)}</div>
+          <div>MAX за хит {userScoreArr.length > 0 && Math.max(...userScoreArr)}</div>
+          <div>Лучший Aim {aimResults.length > 0 && Math.min(...aimResults)}</div>
           <div>осталось времени { 120 - timeCount} </div>
           <button
             className='eighthGameScreen__gameWrapper__optionsBar__button'
